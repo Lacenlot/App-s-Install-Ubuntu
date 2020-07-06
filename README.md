bcb658bfcb3d735a45bd71d9b25991702e6ad408
Lacenlot <gabrieloliveira.17@outlook.com>
2020-07-06 18:25:39 -0300

    validations

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # App-s-Install-Ubuntu
-A simple script for the first installation of app`s in freshly installed SO(Linux).
+A simple script for the first installation of app`s in freshly installed Ubuntu(19.10/20.04).
--- firstinstall.sh
+++ firstinstall.sh
@@ -3,6 +3,24 @@
 PPA_LUTRIS="ppa:lutris-team/lutris"
 GOOGLE_CHROME_LINK="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 DROPBOX_LINK="https://www.dropbox.com/download?plat=lnx.x86_64"
+SNAP_APPS=(
+ code
+ spotify
+ discord
+ postman
+ pulseaudio
+)
+
+if ! ping -c 5 8.8.8.8 -q &> /dev/null; then
+  echo "COMPUTADOR SEM CONEXÃO COM A INTERNET!"
+  exit 1
+fi
+
+if [[ ! -x `which wget` ]]; then
+  sudo apt install wget -y
+else
+  echo "WGET JÁ ESTÁ INSTALADO!"
+fi
 
 remove_locks () {
   sudo rm /var/lib/dpkg/lock/frontend
@@ -15,45 +33,53 @@ repository_lutris () {
 
 add_architecture () {
   sudo dpkg --add-architecture i386
+  sudo apt update -y
 }
 
-install_google () {
-  wget "$GOOGLE_CHROME_LINK" -P $HOME/Downloads
-  sudo dpkg -i $HOME/Downloads/*.deb
+install_google_chrome () {
+  parser_url=$(echo ${GOOGLE_CHROME_LINK##*/} sed 's/-/_/g' | cut -d _ -f 1)
+  if ! dpkg -l | grep -iq $parser_url; then
+    wget -c "$GOOGLE_CHROME_LINK" -P $HOME/Downloads
+    sudo dpkg -i $HOME/Downloads/${GOOGLE_CHROME_LINK##*/}
+  else
+    echo "GOOGLE CHROME JÁ ESTÁ INSTALADO!"
+  fi
   sudo apt -f install -y
-  #wget -O - "$DROPBOX_LINK" | tar xzf -
-  #~/.dropbox-dist/dropboxd
 }
+
+#wget -O - "$DROPBOX_LINK" | tar xzf -
+#~/.dropbox-dist/dropboxd
 
 install_java_eclipse () {
-  sudo apt install openjdk-11-jre-headless #version 11.0.7+10-2ubuntu2~19.10
+  if ! dpkg -l | grep -q openjdk-11-jre-headless; then
+    sudo apt install openjdk-11-jre-headless -y #version 11.0.7+10-2ubuntu2~19.10
+  else
+    echo "JAVA JÁ ESTÁ INSTALADO!"
+  fi
   sudo snap install --classic eclipse
 }
 
 install_snap_apps () {
-  sudo snap install --classic code
-  sudo snap install spotify
-  sudo snap install discord
-  sudo snap install pulseaudio
-}
-
-update_apt () {
-  sudo apt update -y
+  for apps in ${SNAP_APPS[@]}; do
+    if ! snap list | grep -q $apps; then
+      [[ $apps == code ]] && sudo snap install --classic code
+      [[ $apps != code ]] && sudo snap install $apps
+    else
+      echo "O SNAP JÁ ESTÁ INSTALADO - " $apps
+    fi
+  done
 }
 
 upgrade_clear () {
-  sudo apt update
-  ##sudo apt update && sudo apt dist-upgrade -y
-  sudo apt autoclean
-  sudo apt autoremove -y
+  sudo apt update && sudo apt autoclean && sudo apt autoremove -y
+  ##sudo apt update && sudo apt dist-upgrade -y // checar se tem att da distro!
 }
 
 cd ~
 remove_locks
+add_architecture
 repository_lutris
-update_apt
-install_google
+install_google_chrome
 install_java_eclipse
 install_snap_apps
 upgrade_clear
-
